4912498fa6e37ea757c017467f549f50a180de90
7yuri7 <Mad.iruy@proton.me>
2026-02-28 19:02:30 -0400

    Uma pequena vibecodada dos snippets do vscode,(Funcionou de primeira )

--- avg.cpp
+++ avg.cpp
@@ -2,9 +2,8 @@
 #include <string>
 
 using namespace std;
-
-int main(){
-  string nomeAluno;
+void media(){
+   string nomeAluno;
   double nota1, nota2, nota3, media;
 
   cout << "Digite nome do aluno: ";
@@ -28,8 +27,34 @@ int main(){
   bool aprovado = media >= 6.0;
   cout << "Aprovado? (1 para sim, 0 para não): " << aprovado << endl;
   cout << "Nota 3: " << nota3 << endl;// feito para testar a saida do valor 2
+}
+
+void menu(){
+  int opcao;
+  cout << "__MENU__" << endl;
+  cout << "1. Calcular média" << endl;
+  cout << "2. Sair" << endl;
+  cout << "Escolha uma opção: ";
+  cin >> opcao;
+  switch(opcao){
+    case 1:
+      media();
+      break;
+    case 2:
+      cout << "Saindo do programa..." << endl;
+      exit(0);
+    default:
+      cout << "Opção inválida. Tente novamente." << endl;
+  }
+}
+
+
+
+
+int main(){
+  do{
+    menu();
+  }while(true);
+ 
   return 0;
-
-
-
 }
